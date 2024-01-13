1e94feca30f48317df95b40eaf507b4d72022a0e
LrnvAyAnA <nyanya.yara@gmail.com>
2024-01-13 23:20:29 +0500

    oppotunity to import svg

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { ImageBackground,View, Text,StyleSheet,TouchableOpacity, Button,Image, TouchableHighlight } from 'react-native';
+import OrangeBut from '../assets/buttonFromMain.svg';
 
 export default function Welcome() {
+  const text = 'изучай\nматематику\nвесело\nи эффективно!';
     return (
-      <View style={styles.container}>
-        <Text>This is the Home screen</Text>
-      </View>
+      <ImageBackground source={require('../assets/bg.jpg')}style={styles.background}>
+        <View style={styles.container}>
+        <Text style={styles.text}>{text.toUpperCase()}</Text>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity>
+            <OrangeBut width={337} height={87} />
+          <Text style={styles.buttonText}>Продолжить</Text>
+            </TouchableOpacity>
+        </View>
+        </View>
+      </ImageBackground>
     );
   }
   const styles = StyleSheet.create({
-    container: {
+    background: {
       flex: 1,
-      backgroundColor:'#ffffff',
-      justifyContent: 'center',
-      alignItems: 'center',
+      resizeMode: 'cover',
+    },
+    container: {
+      // flex: 1,
+      // alignItems: 'flex-start',
+    },
+    text:{
+      color: '#ffffff',
+      fontSize:36,
+      marginLeft:24,
+      marginTop:400,
+    },
+    buttonContainer:{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',      
+    },
+    buttonText: {
+      marginTop:-50,
+      color: '#fff',
+      fontSize: 22,
+      alignSelf:'center'
     },
   });
